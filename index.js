ed1b0e3ac8975e0286ccebcff7d26a07a37b593e
kabbi <userdima@gmail.com>
2021-04-25 15:58:41 +0300

    Remove connection manager from public export for now

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  ConnectionManager: require('./src/connection-manager'),
   NetworkLayer: require('./src/layers/network-layer'),
   LowerLayer: require('./src/layers/lower-transport-layer'),
   UpperLayer: require('./src/layers/upper-transport-layer'),
